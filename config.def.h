4dc2d95981cb6564a188abacd18a5c67bc14d6b7
NonameBlank007 <104923373+NonameBlank007@users.noreply.github.com>
2025-11-18 22:07:37 +0530

    dwm: patch: anybar-polybar-tray-fix

--- config.def.h
+++ config.def.h
@@ -11,6 +11,10 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const char *alttrayname      = "tray";    /* Polybar tray instance name */
+static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = {"FiraCodeNerdFont:size=12", "JetBrainsMono Nerd Font:size=12"};
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 static unsigned int baralpha        = 0xd0;
